1d5403b52dec4c4513d1bc901245ca4b52e29c03
bigblue316 <48892840+bigblue316@users.noreply.github.com>
2021-12-29 22:35:36 -0600

    Update hot_keys.py

--- hot_keys/hot_keys.py
+++ hot_keys/hot_keys.py
@@ -3,8 +3,8 @@ from webhook import send_webhook
 
 headers = {
     'accept': 'application/json, text/plain, */*',
-    'authorization': 'Token 2cd4f02171217c7049f1237c9b6485f317c48d87',
-    'api-key': '7c625c136abaac5fb5aa6f98b3a9196a0ef80c47',
+    'authorization': 'Token_here',
+    'api-key': 'api_key_here',
     'user-agent': 'Mozilla/5.0 (Linux; Android 6.0.1; KIW-L24 Build/HONORKIW-L24; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.152 Mobile Safari/537.36',
     'origin': 'http://localhost',
     'x-requested-with': 'com.keycollectorcomics.keycollector',
@@ -19,7 +19,7 @@ headers = {
 params = (
     ('version', '3.1.4'),
     ('search', ''),
-    ('category', 805),
+    ('category', 80),
     ('limit', '1'),
     ('offset', '0'),
 )
@@ -33,7 +33,7 @@ try:    #  get id's  #
     for new_id_list in parsed_json['results']:
         new_id = (new_id_list['id'])
 
-    with open('/home/ihofdomah/key_collectors_project/hot_keys/latest.json') as access_json:
+    with open('./key_collectors_project/hot_keys/latest.json') as access_json:
         old_json = json.load(access_json)
 
     for old_id_list in old_json['results']:
@@ -43,12 +43,12 @@ try:    #  get id's  #
     if new_id != old_id:
         print('Found update')
         # send_webhook()
-        with open("/home/ihofdomah/key_collectors_project/hot_keys/latest.json", "w") as file:            ## WRITE JSON FILE ##
+        with open("./key_collectors_project/hot_keys/latest.json", "w") as file:            ## WRITE JSON FILE ##
             file.write(json.dumps(parsed_json))
     else:
         print('No update')
 
 except:
     print('First file created')
-    with open("/home/ihofdomah/key_collectors_project/hot_keys/latest.json", "w") as file:    ## WRITE JSON FILE ##
+    with open("./key_collectors_project/hot_keys/latest.json", "w") as file:    ## WRITE JSON FILE ##
         file.write(json.dumps(parsed_json))
